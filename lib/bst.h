2875be8f21940bc7304fed814820b0bc9538aa2c
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2026-02-21 14:11:55 +0300

    Formatted code, implemented bstInsert(), bstContains()

--- lib/bst.h
+++ lib/bst.h
@@ -3,15 +3,15 @@
 
 typedef struct BST BST;
 
-BST *bstNew();
-void bstInsert(BST *, int);
-bool bstContains(BST *, int);
-void bstFree(BST **);
+BST* bstNew(int);
+bool bstInsert(BST*, int);
+bool bstContains(BST*, int);
+void bstFree(BST**);
 
-void bstInorder(BST *tree);
-void bstPreorder(BST *tree);
-void bstPostorder(BST *tree);
-int bstHeight(BST *tree);
-int bstSize(BST *tree);
-int bstMin(BST *tree);
-int bstMax(BST *tree);
+void bstInorder(BST* tree);
+void bstPreorder(BST* tree);
+void bstPostorder(BST* tree);
+int bstHeight(BST* tree);
+int bstSize(BST* tree);
+int bstMin(BST* tree);
+int bstMax(BST* tree);
