92d3064cc45af5f1295abe8f714a499f29728256
Georgiy Mikhailenko <kislball@yahoo.com>
2026-02-21 15:12:53 +0300

    Add simple tests

---
+++ tests/test.c
@@ -0,0 +1,22 @@
+#include "bst.h"
+#include <assert.h>
+#include <stdio.h>
+
+int main()
+{
+	BST* tree = bstNew(25);
+	bstInsert(tree, 15);
+	bstInsert(tree, 20);
+	bstInsert(tree, 503);
+	bstInsert(tree, 8);
+
+	assert(bstContains(tree, 15));
+	assert(bstContains(tree, 20));
+	assert(bstContains(tree, 503));
+	assert(bstContains(tree, 8));
+	assert(bstMin(tree) == 8);
+	assert(bstMax(tree) == 503);
+	assert(bstSize(tree) == 4);
+
+	return 0;
+}
